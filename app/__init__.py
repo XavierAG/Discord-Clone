60f2db77074b6cbae6f2ef9bd27cb2cebc928ac9
Jashan2233 <jashan17100@gmail.com>
2023-09-22 18:26:27 -0500

    merged models

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .seeds import seed_commands
 from .config import Config
-
+#app
 app = Flask(__name__, static_folder='../react-app/build', static_url_path='/')
 
 # Setup login manager
