cbb13558e7136aacbbc23ee664ca4f8b6726ccf1
Jashan2233 <jashan17100@gmail.com>
2023-09-23 16:17:42 -0500

    route for editing servers and websockets implemented

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request, session, redirect
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect, generate_csrf
+from .socket import socketio
 from flask_login import LoginManager
 from .models import db, User
 from .api.user_routes import user_routes
@@ -32,6 +33,7 @@ app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(server_routes, url_prefix='/api/servers')
 db.init_app(app)
 Migrate(app, db)
+socketio.init_app(app) # Initiating Sockets
 
 # Application Security
 CORS(app)
--- app/api/server_routes.py
+++ app/api/server_routes.py
@@ -1,6 +1,7 @@
 from .auth_routes import validation_errors_to_error_messages
 from flask import Blueprint, jsonify, session, request
 from flask_login import login_required, current_user
+from app.socket import handle_add_channel, handle_edit_server, handle_delete_server, handle_add_server
 from app.models import Server, User, db
 from app.forms import ServerForm, ChannelForm
 
@@ -15,7 +16,7 @@ def get_all_servers():
     """
     Query a list of all servers not set to private by an authorized user
     """
-    public_servers = Server.query.filter(Server.private== False).all()
+    public_servers = Server.query.filter(Server.private == False).all()
     return {'servers': [server.to_dict() for server in public_servers]}
 
 
@@ -33,9 +34,35 @@ def create_server():
         )
         db.session.add(new_server)
         db.session.commit()
+        #Emiting Data to the Server from Client when a new Server is made by a Client
+        handle_add_server(new_server.to_dict())
+        return 'Server created successfully'
 
     if form.errors:
         print(form.errors)
         return { "message": "errors"}
 
     return { "message": "Server created!"}
+
+
+#Editing Servers
+@server_routes.route('/<int:server_id>', methods=['PUT'])
+@login_required
+def edit_server(server_id):
+    form = ServerForm()
+
+    server = Server.query.get(server_id)
+
+    if form.validate_on_submit() and server.owner_id == current_user.id:
+        server.name = form.data['name'],
+        server.image_url = form.data['image_url'],
+        server.private = form.data['private']
+        db.session.commit()
+
+        #Emit a Socket Event to notify clients about the Server Edit
+        handle_edit_server(server.to_dict())
+
+        return server.to_dict()
+    if form.errors:
+        print(form.errors)
+        return{'message': 'errors'}
