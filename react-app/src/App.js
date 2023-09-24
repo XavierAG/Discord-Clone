5a6b0c2894f7748ce3fa06f2f04d38d77cbda590
cmstucke <cmstucke@gmail.com>
2023-09-24 08:15:39 -0400

    split home page render by store user

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import SignupFormPage from "./components/SignupFormPage";
 import LoginFormPage from "./components/LoginFormPage";
@@ -7,34 +7,54 @@ import PublicServers from "./components/PublicServers";
 import { authenticate } from "./store/session";
 // import Navigation from "./components/Navigation";
 import LandingPage from "./components/LandingPage";
+import Dashboard from "./components/Dashboard";
 // import WebSockets from "./components/WebSockets";
 
 function App() {
   const dispatch = useDispatch();
+  const userState = useSelector(state => state.session.user);
+  console.log('USER STATE:', userState);
   const [isLoaded, setIsLoaded] = useState(false);
   useEffect(() => {
     dispatch(authenticate()).then(() => setIsLoaded(true));
   }, [dispatch]);
 
+  const unauthenticated = (
+    <Switch>
+      <Route path="/login">
+        <LoginFormPage />
+      </Route>
+      <Route path="/signup">
+        <SignupFormPage />
+      </Route>
+      <Route exact path="/">
+        <LandingPage />
+      </Route>
+      <Route exact path="/servers">
+        <PublicServers />
+      </Route>
+    </Switch>
+  )
+
+  const authenticated = (
+    <Switch>
+      <Route exact path="/">
+        <Dashboard />
+      </Route>
+      <Route exact path="/servers">
+        <PublicServers />
+      </Route>
+    </Switch>
+  )
+
   return (
     <>
-      {/* <Navigation isLoaded={isLoaded} /> */}
-      {isLoaded && (
-        <Switch>
-          <Route path="/login">
-            <LoginFormPage />
-          </Route>
-          <Route path="/signup">
-            <SignupFormPage />
-          </Route>
-          <Route exact path="/">
-            <LandingPage />
-          </Route>
-          <Route exact path="/servers">
-            <PublicServers />
-          </Route>
-        </Switch>
-      )}
+      {isLoaded &&
+        !userState &&
+        unauthenticated}
+      {isLoaded &&
+        userState &&
+        authenticated}
     </>
   );
 }
