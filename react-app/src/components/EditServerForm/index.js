4f6cb58879133daf1b483d3d64a9d521714cca86
Jashan2233 <jashan17100@gmail.com>
2023-09-25 16:30:26 -0500

    merged dev

--- react-app/src/components/EditServerForm/index.js
+++ react-app/src/components/EditServerForm/index.js
@@ -5,87 +5,86 @@ import { editServerThunk } from "../../store/servers";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 export const EditServerForm = () => {
-  const sessionUser = useSelector((state) => state.session.user)
-  const history = useHistory()
+  const sessionUser = useSelector((state) => state.session.user);
+  const history = useHistory();
   const { server_id } = useParams();
-  const [name, setName] = useState('')
-  const [imageUrl, setImageUrl] = useState('')
-  const [isPrivate, setIsPrivate] = useState(false)
+  const [name, setName] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [isPrivate, setIsPrivate] = useState(false);
   const dispatch = useDispatch();
 
-
+  // Handle Submitting
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const serverData = {name, image_url:imageUrl, private:isPrivate}
+    const serverData = { name, image_url: imageUrl, private: isPrivate };
 
     const response = await dispatch(editServerThunk(server_id, serverData));
-  
-    history.push(`/servers/${server_id}`)
+
+    history.push(`/servers/${server_id}`);
   };
 
   useEffect(() => {
-    const fetchServer = async () =>{
+    const fetchServer = async () => {
       try {
-        const response = await fetch(`/api/servers/${server_id}`)
+        const response = await fetch(`/api/servers/${server_id}`);
         if (response.ok) {
-          const data = await response.json()
-          console.log('this is the old server data: ',data.server);
+          const data = await response.json();
+          console.log("this is the old server data: ", data.server);
           if (data && data.server) {
-            const oldServer = data.server
-            setName(oldServer.name)
-            setImageUrl(oldServer.image_url)
-            setIsPrivate(oldServer.private)
+            const oldServer = data.server;
+            setName(oldServer.name);
+            setImageUrl(oldServer.image_url);
+            setIsPrivate(oldServer.private);
           }
-        }
-        else{
+        } else {
           console.log(server_id);
-          throw new Error('failed to fetch server data')
+          throw new Error("failed to fetch server data");
         }
       } catch (error) {
         console.log(error);
       }
-    }
-    fetchServer()
-  },[server_id])
+    };
+    fetchServer();
+  }, [server_id]);
 
   return (
     <div>
-    <h2>Edit your Server</h2>
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="name">Server Name:</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          value={name}
-          onChange={e => setName(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="image_url">Image URL:</label>
-        <input
-          type="text"
-          id="image_url"
-          name="image_url"
-          value={imageUrl}
-          onChange={e => setImageUrl(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="private">Private:</label>
-        <input
-          type="checkbox"
-          id="private"
-          name="private"
-          checked={isPrivate}
-          onChange={e => setIsPrivate(e.target.checked)}
-        />
-      </div>
-      <button type="submit">Update Server</button>
-    </form>
-  </div>
+      <h2>Edit your Server</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Server Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="image_url">Image URL:</label>
+          <input
+            type="text"
+            id="image_url"
+            name="image_url"
+            value={imageUrl}
+            onChange={(e) => setImageUrl(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="private">Private:</label>
+          <input
+            type="checkbox"
+            id="private"
+            name="private"
+            checked={isPrivate}
+            onChange={(e) => setIsPrivate(e.target.checked)}
+          />
+        </div>
+        <button type="submit">Update Server</button>
+      </form>
+    </div>
   );
 };
 
