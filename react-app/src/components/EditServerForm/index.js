c07904d3236c50bd5fa9b7f284e19e445846beb5
Jashan2233 <jashan17100@gmail.com>
2023-09-25 11:23:18 -0500

    issues solved

--- react-app/src/components/EditServerForm/index.js
+++ react-app/src/components/EditServerForm/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch } from "react-redux";
@@ -30,6 +31,61 @@ export const EditServerForm = () => {
     // </div>
     <>
     </>
+=======
+// AddServerForm.js
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { editServerThunk } from "../../store/servers";
+
+const EditServerForm = () => {
+  const dispatch = useDispatch();
+  const [serverData, setServerData] = useState({
+    name: "",
+    image_url: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setServerData({ ...serverData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(editServerThunk(serverData));
+    setServerData({
+      name: "",
+      image_url: "",
+    });
+  };
+
+  return (
+    <div>
+      <h2>Edit Server</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Server Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={serverData.name}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="image_url">Image URL:</label>
+          <input
+            type="text"
+            id="image_url"
+            name="image_url"
+            value={serverData.image_url}
+            onChange={handleInputChange}
+          />
+        </div>
+        <button type="submit">Create Server</button>
+      </form>
+    </div>
+>>>>>>> 77495e8 (forms for testing put/posts routes added)
   );
 };
 
