d5f5e688c40b30312e821b50822a9396ae998ecf
Jashan2233 <jashan17100@gmail.com>
2023-09-26 08:04:20 -0500

    added servers store back after loss

---
+++ react-app/src/store/channel.js
@@ -0,0 +1,6 @@
+// Channel Consts
+
+const GET_CHANNELS = "servers/GET_CHANNELS";
+const POST_CHANNEL = "servers/POST_CHANNEL";
+const EDIT_CHANNEL = "servers/EDIT_CHANNEL";
+const DELETE_CHANNEL = "servers/DELETE_CHANNEL";
