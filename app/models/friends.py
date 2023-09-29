7000094eb74817f0eb9b6c956b2cf6be134542d9
Xavier Guzman <xavierart2001@gmail.com>
2023-09-29 13:32:39 -0500

    added table args

--- app/models/friends.py
+++ app/models/friends.py
@@ -1,6 +1,8 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 
 # Define the association table for the many-to-many relationship
+if environment == "production":
+    __table_args__ = {'schema': SCHEMA}
 friends = db.Table(
     'friends',
 
@@ -8,6 +10,3 @@ friends = db.Table(
     db.Column('friend_id', db.Integer, db.ForeignKey(add_prefix_for_prod('users.id'))),
     db.Column('the_friend_id', db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')))
 )
-
-if environment == "production":
-    __table_args__ = {'schema': SCHEMA}
