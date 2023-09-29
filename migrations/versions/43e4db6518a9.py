0972849f5be53417d440fb7783951be9004b8e57
Jashan2233 <jashan17100@gmail.com>
2023-09-29 10:37:19 -0500

    added requirements to env and migration files

--- migrations/versions/43e4db6518a9.py
+++ migrations/versions/43e4db6518a9.py
@@ -7,6 +7,9 @@ Create Date: 2023-09-28 19:42:02.355938
 """
 from alembic import op
 import sqlalchemy as sa
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 
 # revision identifiers, used by Alembic.
@@ -63,7 +66,11 @@ def upgrade():
     sa.PrimaryKeyConstraint('id')
     )
     # ### end Alembic commands ###
-
+    if environment == "production":
+        op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE servers SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE channels SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE messages SET SCHEMA {SCHEMA};")
 
 
 def downgrade():
