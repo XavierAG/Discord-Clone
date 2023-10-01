9570669a7bb3cd7e102c905d0182c5a53c5fe1c3
Jashan2233 <jashan17100@gmail.com>
2023-10-01 09:55:08 -0500

    fixed chat on render

--- app/socket.py
+++ app/socket.py
@@ -5,8 +5,8 @@ import os
 # configure cors_allowed_origins
 if os.environ.get('FLASK_ENV') == 'production':
     origins = [
+        'https://discord-clone2-oqdu.onrender.com'
         'https://Biscord.onrender.com',
-        'https://Biscord.onrender.com'
     ]
 else:
     origins = "*"
